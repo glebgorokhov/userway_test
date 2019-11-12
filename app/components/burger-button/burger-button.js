aa556e0324372fadc486ae586657afadda6e813a
Gleb Gorokhov <gg@verstaburo.com>
2019-11-12 14:24:58 +0300

    Update 2019-11-12T11:24:53.638Z

--- app/components/burger-button/burger-button.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-const $ = window.$;
-
-import {openMenu} from "../menu/menu";
-
-export function burgerButton () {
-  $(document).on('click', '.js-burger-button', openMenu);
-}
-/* eslint-enable */
